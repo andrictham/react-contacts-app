e867dde48ffe22ecd11ed06e9bbf0037446a6893
Andric Tham <andric@nuisance.co>
2017-10-29 13:38:00 +0800

    <ListContacts />: Refactor to Stateless Functional Component

--- client/src/ListContacts.js
+++ client/src/ListContacts.js
@@ -1,31 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class ListContacts extends React.Component {
-	render() {
-		const { contacts } = this.props
-		return (
-			<ol className="contact-list">
-				{contacts.map(contact => {
-					return (
-						<li key={contact.id} className="contact-list-item">
-							<div
-								className="contact-avatar"
-								style={{
-									backgroundImage: `url(${contact.avatarURL})`,
-								}}
-							/>
-							<div className="contact-details">
-								<p>{contact.name}</p>
-								<p>{contact.email}</p>
-							</div>
-							<button className="contact-remove" />
-						</li>
-					)
-				})}
-			</ol>
-		)
-	}
+const ListContacts = ({ contacts }) => {
+	return (
+		<ol className="contact-list">
+			{contacts.map(contact => {
+				return (
+					<li key={contact.id} className="contact-list-item">
+						<div
+							className="contact-avatar"
+							style={{
+								backgroundImage: `url(${contact.avatarURL})`,
+							}}
+						/>
+						<div className="contact-details">
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+						<button className="contact-remove" />
+					</li>
+				)
+			})}
+		</ol>
+	)
 }
 
 ListContacts.propTypes = {
