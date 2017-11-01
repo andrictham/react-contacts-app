62a11229e87eab0e306721e4bab61ccb78ff4e55
Andric Tham <andric@nuisance.co>
2017-11-01 09:52:00 +0800

    <ListContacts />: Hook up real API

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 	constructor(props) {
@@ -8,26 +9,7 @@ class App extends Component {
 	}
 
 	state = {
-		contacts: [
-			{
-				id: 'ryan',
-				name: 'Ryan Florence',
-				email: 'ryan@reacttraining.com',
-				avatarURL: 'http://localhost:5001/ryan.jpg',
-			},
-			{
-				id: 'michael',
-				name: 'Michael Jackson',
-				email: 'michael@reacttraining.com',
-				avatarURL: 'http://localhost:5001/michael.jpg',
-			},
-			{
-				id: 'tyler',
-				name: 'Tyler McGinnis',
-				email: 'tyler@reacttraining.com',
-				avatarURL: 'http://localhost:5001/tyler.jpg',
-			},
-		],
+		contacts: [],
 	}
 
 	removeContact = selectedContact => {
@@ -38,6 +20,16 @@ class App extends Component {
 				contact => contact.id !== selectedContact.id,
 			),
 		}))
+
+		ContactsAPI.remove(selectedContact)
+	}
+
+	componentDidMount() {
+		ContactsAPI.getAll().then(contacts => {
+			this.setState({
+				contacts,
+			})
+		})
 	}
 
 	render() {
