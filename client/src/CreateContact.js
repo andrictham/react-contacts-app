c24b8cfda475fafbd3d66c1228b698c84ac9186f
Andric Tham <andric@nuisance.co>
2017-11-04 13:32:35 +0800

    <CreateContact />: Scaffold form with provided <ImageInput /> component

--- client/src/CreateContact.js
+++ client/src/CreateContact.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component {
 	render() {
-		return <div>Create contact</div>
+		return (
+			<div>
+				<Link className="close-create-contact" to="/">
+					Close
+				</Link>
+				<form action="" className="create-contact-form">
+					<ImageInput
+						className="create-contact-avatar-input"
+						name="avatarURL"
+						maxHeight={128}
+					/>
+					<div className="create-contact-details">
+						<input type="text" name="name" placeholder="Name" />
+						<input type="text" name="name" placeholder="Email" />
+						<button>Add Contact</button>
+					</div>
+				</form>
+			</div>
+		)
 	}
 }
 
